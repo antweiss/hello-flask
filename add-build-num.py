a489b3ed240840cd42557e23b92f497a299508f2
Ant Weiss <ant.weiss@gmail.com>
2021-08-04 14:12:52 +0300

    Update add-build-num.py

--- add-build-num.py
+++ add-build-num.py
@@ -3,6 +3,6 @@ import fileinput
 import sys
 
 
-print "Build number is " + sys.argv[1]
+print("Build number is " + sys.argv[1])
 for line in fileinput.input("application.py", inplace=True):
   print(line.replace('Hello World!', 'Hello World! - '+sys.argv[1]).rstrip())
